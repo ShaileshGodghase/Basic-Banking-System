f38a0c53aa799ed199fca452045d36ac30745428
Shailesh Godghase <75568299+ShaileshGodghase@users.noreply.github.com>
2021-07-10 17:06:49 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,12 +5,6 @@ $username="root";
 $password="";
 $db="sbg_bank";
 
-/*
-$server="sql308.epizy.com";
-$username="epiz_29095374";
-$password="CK3fFloFdxx";
-$db="epiz_29095374_SBGBank";
-*/
 $conn=mysqli_connect($server,$username,$password,$db);
 
 if($conn){
